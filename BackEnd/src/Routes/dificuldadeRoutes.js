6c60d87687ce97f2190c332e2dfe93061bb24e65
marceladacruz779-sys <marceladacruz779@gmail.com>
2026-05-25 08:22:38 -0300

    3

--- BackEnd/src/Routes/dificuldadeRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-
-const DificuldadeController = require('../Controllers/dificuldadeControllers');
-
-router.get('/Dificuldade', DificuldadeController.l);
-
-module.exports = router;
