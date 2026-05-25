7207d3a8ece85bb2ae04dd797a7c458d85098769
Beatryz Caroline dos Santos Rodrigues <beatryzsenai2312@gmail.com>
2026-05-25 08:08:59 -0300

    add area routes for listing all entries

---
+++ BackEnd/src/Routes/areaRoutes.js
@@ -0,0 +1,14 @@
+// Importar o Express para criar o router
+const express = require('express');
+const router = express.Router();
+
+// Importar as funções do Controller
+const AreaController = require('../controllers/areaControllers');
+
+// DEFINIÇÃO DAS ROTAS
+
+// GET /area -- listar todos
+router.get('/', AreaController.listarTodos);
+
+// EXPORTAR O ROUTER
+module.exports = router;
