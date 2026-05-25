b178ee5ade24a540eb70822337620b5908b3566a
Beatryz Caroline dos Santos Rodrigues <beatryzsenai2312@gmail.com>
2026-05-25 08:18:14 -0300

    update area route to use filtragem method and add new image asset

--- BackEnd/src/Routes/areaRoutes.js
+++ BackEnd/src/Routes/areaRoutes.js
@@ -8,7 +8,7 @@ const AreaController = require('../controllers/areaControllers');
 // DEFINIÇÃO DAS ROTAS
 
 // GET /area -- listar todos
-router.get('/', AreaController.listarTodos);
+router.get('/area', AreaController.filtragem);
 
 // EXPORTAR O ROUTER
 module.exports = router;
