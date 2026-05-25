d07134431cf91629f5bae797a052a0ed3783eee9
marceladacruz779-sys <marceladacruz779@gmail.com>
2026-05-25 08:20:30 -0300

    certo

--- BackEnd/src/Routes/dificuldadeRoutes.js
+++ BackEnd/src/Routes/dificuldadeRoutes.js
@@ -1,17 +1,9 @@
-<<<<<<< Updated upstream
+
 const express = require('express');
 const router = express.Router();
 
 const DificuldadeController = require('../Controllers/dificuldadeControllers');
 
 router.get('/Dificuldade', DificuldadeController.l);
-=======
- const express = require('express');
-const router = express.Router();
-const dificuldadeController = require('../Controllers/dificuldadeControllers');
-
-router.get('/dificuldade', dificuldadeController.l);
->>>>>>> Stashed changes
-
 
 module.exports = router;
